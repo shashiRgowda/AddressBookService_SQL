490031f0293b17168a8a71538a4a919edfeb47e0
shashiRgowda <shashikumartr64@gmail.com>
2021-06-07 19:53:25 +0530

    Added:EditExistingContacts

---
+++ Addressbooks.sql.sql
@@ -0,0 +1,28 @@
+create database Address_Book_Service
+
+select * from sys.databases;
+
+use Address_Book_Service
+
+create table AddressBook
+(
+firstname varchar(30) not null,
+lastname varchar(30) not null,
+address varchar(30) not null,
+city varchar(30) not null,
+state varchar(30) not null,
+zip int not null,
+phonenumber varchar(15) not null,
+email varchar(30) not null,
+);
+
+select * from AddressBook
+
+insert into AddressBook values
+('Shashi','Gowdru','KGF','Kolar','Karnataka',563101,8884540867,'shashi1232gmail.com'),
+('Ashok','Chakra','Chintamani','Chickkaballapure','Karnataka',563125,8884540867,'shashi1232gmail.com'),
+('Sohan','Reddy','vijayaNagara','Bengalore','Karnataka',563125,8884540867,'shashi1232gmail.com');
+
+update AddressBook set zip = 563101 where firstname = 'shashi'
+
+select * from AddressBook
